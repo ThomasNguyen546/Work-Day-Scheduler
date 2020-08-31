f799460a08137efc405d83aa2a76230a076d0c25
Thomas Nguyen <thomasnguyen546@gmail.com>
2020-08-30 22:34:21 -0600

    Missing hourTracker colors

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-$(document).ready(function() {
-    // Display date at the top
-    $("#currentDay").text(moment().format("MMMM Do, YYYY"));
-    
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        //set items in local storage.
-        localStorage.setItem(time, text);
-    })
-
-
-// Will load the data for each hour/row
+// Load the data for each hour/row
 $("#hour1 .description").val(localStorage.getItem("hour1"));
 $("#hour2 .description").val(localStorage.getItem("hour2"));
 $("#hour3 .description").val(localStorage.getItem("hour3"));
@@ -22,22 +9,24 @@ $("#hour7 .description").val(localStorage.getItem("hour7"));
 $("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 
-
+$(document).ready(function() {
+    // Display date at the top
+    $("#currentDay").text(moment().format("MMMM Do, YYYY, h:mm a"));
+    
+//Checking the time to determine whether present, past, or future
 function hourTracker() {
-    //get current number of hours.
+    // Current number of hours
     var currentHour = moment().hour();
 
-    // loop over time blocks
-    $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-
+    // Loop to determine whether it'll be present, past, or future
+    $(".9").each(function() {
         //check if we've moved past this time
-        if (blockHour < currentHour) {
+        if (9 < currentHour) {
             $(this).addClass("past");
             $(this).removeClass("future");
             $(this).removeClass("present");
         }
-        else if (blockHour === currentHour) {
+        else if (9 === currentHour) {
             $(this).removeClass("past");
             $(this).addClass("present");
             $(this).removeClass("future");
@@ -48,6 +37,145 @@ function hourTracker() {
             $(this).addClass("future");
         }
     })
+    $(".10").each(function() {
+        //check if we've moved past this time
+        if (10 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (10 === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    $(".11").each(function() {
+        //check if we've moved past this time
+        if (11 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (11 === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    $(".12").each(function() {
+        //check if we've moved past this time
+        if (12 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (12 === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    $(".13").each(function() {
+        //check if we've moved past this time
+        if (13 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (13 == currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else { 
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    $(".14").each(function() {
+        //check if we've moved past this time
+        if (14 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (14 == currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    $(".15").each(function() {
+        //check if we've moved past this time
+        if (15 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (15 === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    $(".16").each(function() {
+        //check if we've moved past this time
+        if (16 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (16 === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+    $(".17").each(function() {
+        //check if we've moved past this time
+        if (17 < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        } else if (17 === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
 }
 hourTracker();
+
+$(".saveBtn").on("click", function () {
+    event.preventDefault();
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    
+    //set items in local storage.
+    localStorage.setItem(time, text);
 })
+}) 
